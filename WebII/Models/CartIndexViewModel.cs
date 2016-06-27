a236ae9805b976e31b53a6b4d5b33947a0c67af2
yujy <yujy@novasoftware.cn>
2016-06-27 18:15:57 +0800

    ss 8.2

---
+++ WebII/Models/CartIndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Domain.Abstract;
+using Domain.Entities;
+
+namespace WebII.Models
+{
+    public class CartIndexViewModel
+    {
+        public Cart Cart { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
